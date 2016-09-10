9f057327e1b003f37242007b65546e1901b230a3
pktrigg <p.kennedy@fugro.com>
2016-09-10 16:47:56 +0800

    more work on isometric waterfalls

--- pyAllHillShadedWaterfall.py
+++ pyAllHillShadedWaterfall.py
@@ -37,36 +37,13 @@ def main():
     for filename in glob(args.inputFile):
         # navigation = loadNavigation(filename)
         # print (navigation)
-        xResolution,yResolution, beamCount = computeXYResolution(filename)
+        xResolution, yResolution, beamCount, leftExtent, rightExtent = computeXYResolution(filename)
+        print("XRes %.2f YRes %.2f beamCount %d leftExtent %.2f, rightExtent %.2f" % (xResolution, yResolution, beamCount, leftExtent, rightExtent)) 
+        
         if beamCount == 0:
             print ("No data to process, skipping empty file")
             continue
-        createWaterfall(filename, colors, beamCount, float(args.shadeScale), xResolution, yResolution, args.rotate, args.gray)
-
-def loadPal(paletteFileName):
-    '''this will load and return a .pal file so we can apply colors to depths.  It will strip off the headers from the file and return a list of n*RGB values'''
-    colors = []
-    with open(paletteFileName,'r') as f:
-        next(f) # skip headings
-        next(f) # skip headings
-        next(f) # skip headings
-        reader=csv.reader(f,delimiter='\t')
-        for red,green,blue in reader:
-            thiscolor = [float(red)/255.0, float(green) / 255.0, float(blue) / 255.0]
-            colors.append(thiscolor)
-    return colors
-
-def loadNavigation(fileName):    
-    '''loads all the navigation into lists'''
-    navigation = []
-    r = pyall.ALLReader(fileName)
-    while r.moreData():
-        TypeOfDatagram, datagram = r.readDatagram()
-        if (TypeOfDatagram == 'P'):
-            datagram.read()
-            navigation.append([datagram.Time, datagram.Latitude, datagram.Longitude])
-    r.close()
-    return navigation
+        createWaterfall(filename, colors, beamCount, float(args.shadeScale), xResolution, yResolution, args.rotate, args.gray, leftExtent, rightExtent)
 
 def computeXYResolution(fileName):    
     '''compute the approximate across and alongtrack resolution so we can make a nearly isometric Image'''
@@ -80,6 +57,8 @@ def computeXYResolution(fileName):
     recCount = 0
     acrossMeans = np.array([])
     alongIntervals = np.array([])
+    leftExtents = np.array([])
+    rightExtents = np.array([])
     beamCount = 0
     distanceTravelled = 0.0
     while r.moreData():
@@ -98,6 +77,8 @@ def computeXYResolution(fileName):
         if (TypeOfDatagram == 'X') or (TypeOfDatagram == 'D'):
             datagram.read()
             acrossMeans = np.append(acrossMeans, np.average(np.diff(np.asarray(datagram.AcrossTrackDistance))))
+            leftExtents = np.append(leftExtents, datagram.AcrossTrackDistance[0])
+            rightExtents = np.append(rightExtents, datagram.AcrossTrackDistance[-1])
             recCount = recCount + 1
             beamCount = max(beamCount, len(datagram.Depth)) 
             
@@ -109,9 +90,9 @@ def computeXYResolution(fileName):
         return 0,0,0 
     xResolution = np.average(acrossMeans)
     yResolution = distanceTravelled / recCount
-    return xResolution, yResolution, beamCount
+    return xResolution, yResolution, beamCount, np.min(leftExtents), np.max(rightExtents)
 
-def createWaterfall(filename, colors, beamCount, shadeScale=1, xResolution=1, yResolution=1, rotate=False, gray=False):
+def createWaterfall(filename, colors, beamCount, shadeScale=1, xResolution=1, yResolution=1, rotate=False, gray=False, leftExtent=-100, rightExtent=100):
     print ("Processing file: ", filename)
 
     r = pyall.ALLReader(filename)
@@ -119,6 +100,8 @@ def createWaterfall(filename, colors, beamCount, shadeScale=1, xResolution=1, yR
     start_time = time.time() # time the process
     recCount = 0
     waterfall = []
+    stretchedGrid = np.empty( shape=(0, 0) )
+
     isoStretchFactor = math.ceil(xResolution/yResolution)
     print ("xRes %.2f yRes %.2f AcrossStretch %.2f" % (xResolution, yResolution, isoStretchFactor))
     while r.moreData():
@@ -134,35 +117,44 @@ def createWaterfall(filename, colors, beamCount, shadeScale=1, xResolution=1, yR
                 datagram.Depth[d] = datagram.Depth[d] + datagram.TransducerDepth
 
             # we need to stretch the data to make it isometric, so lets use numpy interp routing to do that for Us
-            xp = np.arange(len(datagram.Depth)) #the x distance for the beams of a ping.  we could possibly use teh real values here instead todo
+            xp = np.array(datagram.AcrossTrackDistance) #the x distance for the beams of a ping.  we could possibly use teh real values here instead todo
             fp = np.array(datagram.Depth) #the depth list as a numpy array
-            x = np.linspace(0, len(datagram.Depth), beamCount * isoStretchFactor) #the required samples
+            x = np.linspace(leftExtent, rightExtent, beamCount) #the required samples needs to be about the same as the original number of samples, spread across the across track range
             # x = np.linspace(0, len(datagram.Depth), len(datagram.Depth) * isoStretchFactor) #the required samples
-            newDepths = np.interp(x, xp, fp)
+            newDepths = np.interp(x, xp, fp, left=-1.123, right=-1.123)
             waterfall.insert(0, np.asarray(newDepths))            
 
+
+            # # we need to stretch the data to make it isometric, so lets use numpy interp routing to do that for Us
+            # xp = np.arange(len(datagram.Depth)) #the x distance for the beams of a ping.  we could possibly use teh real values here instead todo
+            # fp = np.array(datagram.Depth) #the depth list as a numpy array
+            # x = np.linspace(0, len(datagram.Depth), beamCount * isoStretchFactor) #the required samples
+            # # x = np.linspace(0, len(datagram.Depth), len(datagram.Depth) * isoStretchFactor) #the required samples
+            # newDepths = np.interp(x, xp, fp)
+            # waterfall.insert(0, np.asarray(newDepths))            
             
             # isoStretchFactor = 1
             # for repeat in range (isoStretchFactor):
             #     waterfall.insert(0, np.asarray(newDepths))            
         recCount += 1
-        # print (recCount, r.currentRecordDateTime())
-        # if recCount==1862:
-        #     print (recCount, r.currentRecordDateTime())
         if r.currentRecordDateTime().timestamp() % 30 == 0:
-            # break
             percentageRead = (recCount / totalrecords) 
             update_progress("Decoding .all file", percentageRead)
     update_progress("Decoding .all file", 1)
 
-    # smooth the surface a little so it looks better
-    # todo
-
+    # we now need to interpolate in the along track direction so we have apprximate isometry
+    npGrid = np.array(waterfall) * shadeScale   
+    for column in npGrid.T:
+        y = np.linspace(0, len(column), len(column) * isoStretchFactor) #the required samples
+        yp = np.arange(len(column)) 
+        w2 = np.interp(y, yp, column, left=0, right=0)
+        stretchedGrid = np.append(stretchedGrid, w2,1)
+    npGrid = stretchedGrid
     meanDepth = np.average(waterfall)
     print ("Mean Depth %.2f" % meanDepth)
     # npGrid = np.array(waterfall) * (200 / meanDepth)   
     if gray:
-        npGrid = np.array(waterfall) * shadeScale * -1.0   
+        # npGrid = np.array(waterfall) * shadeScale * -1.0   
         #Create hillshade a little brighter
         hs = sr.calcHillshade(npGrid, 1, 45, 30)
         hillShadeImage = Image.fromarray(hs).convert('RGBA')
@@ -174,7 +166,7 @@ def createWaterfall(filename, colors, beamCount, shadeScale=1, xResolution=1, yR
         print ("Saved to: ", os.path.splitext(filename)[0]+'.png')
     else:
         #Create hillshade a little darker as we are blending it
-        npGrid = np.array(waterfall) * shadeScale   
+        # npGrid = np.array(waterfall) * shadeScale   
         hs = sr.calcHillshade(npGrid, 1, 45, 5)
         hillShadeImage = Image.fromarray(hs).convert('RGBA')
         # calculate color height map
@@ -256,6 +248,31 @@ def samplesToGrayImageLogarithmic(samples, invert, clip):
     image = Image.fromarray(channel).convert('L')
     
     return image
+
+def loadPal(paletteFileName):
+    '''this will load and return a .pal file so we can apply colors to depths.  It will strip off the headers from the file and return a list of n*RGB values'''
+    colors = []
+    with open(paletteFileName,'r') as f:
+        next(f) # skip headings
+        next(f) # skip headings
+        next(f) # skip headings
+        reader=csv.reader(f,delimiter='\t')
+        for red,green,blue in reader:
+            thiscolor = [float(red)/255.0, float(green) / 255.0, float(blue) / 255.0]
+            colors.append(thiscolor)
+    return colors
+
+def loadNavigation(fileName):    
+    '''loads all the navigation into lists'''
+    navigation = []
+    r = pyall.ALLReader(fileName)
+    while r.moreData():
+        TypeOfDatagram, datagram = r.readDatagram()
+        if (TypeOfDatagram == 'P'):
+            datagram.read()
+            navigation.append([datagram.Time, datagram.Latitude, datagram.Longitude])
+    r.close()
+    return navigation
 
 if __name__ == "__main__":
     main()
