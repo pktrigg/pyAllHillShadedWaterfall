c864c93c6e3cc1664ea90a42891bb2c8248b5a1b
PKLaptop <p.kennedy@fugro.com>
2016-09-17 15:12:43 +0800

    annotation is now optional

--- pyAllHillShadedWaterfall.py
+++ pyAllHillShadedWaterfall.py
@@ -1,24 +1,20 @@
 import sys
 sys.path.append("C:/development/Python/pyall")
 
-import os.path
+import argparse
+import csv
 from datetime import datetime
 import geodetic
-import numpy as np
-import time
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-from PIL import ImageOps
-from PIL import ImageChops
-import math
-import shadedRelief as sr
-from matplotlib import pyplot as plt
-import pyall
 from glob import glob
-import argparse
-import matplotlib.cm as cm
-import csv
+import math
+from matplotlib import pyplot as plt
+from matplotlib import cm
+import numpy as np
+from PIL import Image,ImageDraw,ImageFont, ImageOps, ImageChops
+import pyall
+import shadedRelief as sr
+import time
+import os.path
 import warnings
 
 # ignore numpy NaN warnings when applying a mask to the images.
@@ -29,6 +25,7 @@ def main():
     parser.add_argument('-i', dest='inputFile', action='store', help='-i <ALLfilename> : input ALL filename to image. It can also be a wildcard, e.g. *.all')
     parser.add_argument('-s', dest='shadeScale', default = 1.0, action='store', help='-s <value> : Shade scale factor. A smaller number (0.1) provides less shade that a larger number (10) Range is anything.  [Default: 1.0]')
     parser.add_argument('-z', dest='zoom', default = 1.0, action='store', help='-z <value> : Zoom scale factor. A larger number makes a larger image, and a smaller number (0.5) provides a smaller image, e.g -z 2 makes an image twice the native resolution. [Default: 1.0]')
+    parser.add_argument('-a', action='store_true', default=True, dest='annotate', help='-a : Annotate the image with timestamps.  [Default: True]')
     parser.add_argument('-r', action='store_true', default=False, dest='rotate', help='-r : Rotate the resulting waterfall so the image reads from left to right instead of bottom to top.  [Default is bottom to top]')
     parser.add_argument('-gray', action='store_true', default=False, dest='gray', help='-gray : Apply a gray scale depth palette to the image instead of a color depth.  [Default is False]')
 
@@ -43,12 +40,12 @@ def main():
     print ("processing with settings: ", args)
     for filename in glob(args.inputFile):
         xResolution, yResolution, beamCount, leftExtent, rightExtent, navigation = computeXYResolution(filename)
-        print("XRes %.2f YRes %.2f beamCount %d leftExtent %.2f, rightExtent %.2f" % (xResolution, yResolution, beamCount, leftExtent, rightExtent)) 
+        print("XRes %.2f YRes %.2f beamCount %d leftExtent %.2f, rightExtent %.2f, swathWidth %.2f" % (xResolution, yResolution, beamCount, leftExtent, rightExtent, abs(leftExtent)+abs(rightExtent))) 
         
         if beamCount == 0:
             print ("No data to process, skipping empty file")
             continue
-        createWaterfall(filename, colors, beamCount, float(args.shadeScale), float(args.zoom), xResolution, yResolution, args.rotate, args.gray, leftExtent, rightExtent, navigation)
+        createWaterfall(filename, colors, beamCount, float(args.shadeScale), float(args.zoom), args.annotate, xResolution, yResolution, args.rotate, args.gray, leftExtent, rightExtent, navigation)
 
 def computeXYResolution(fileName):    
     '''compute the approximate across and alongtrack resolution so we can make a nearly isometric Image'''
@@ -99,7 +96,7 @@ def computeXYResolution(fileName):
     yResolution = distanceTravelled / recCount
     return xResolution, yResolution, beamCount, np.min(leftExtents), np.max(rightExtents), navigation
 
-def createWaterfall(filename, colors, beamCount, shadeScale=1, zoom=1.0, xResolution=1, yResolution=1, rotate=False, gray=False, leftExtent=-100, rightExtent=100, navigation = []):
+def createWaterfall(filename, colors, beamCount, shadeScale=1, zoom=1.0, annotate=True, xResolution=1, yResolution=1, rotate=False, gray=False, leftExtent=-100, rightExtent=100, navigation = []):
     print ("Processing file: ", filename)
 
     r = pyall.ALLReader(filename)
@@ -111,7 +108,7 @@ def createWaterfall(filename, colors, beamCount, shadeScale=1, zoom=1.0, xResolu
     maxDepth = -minDepth
     outputResolution = beamCount * zoom
     isoStretchFactor = (yResolution/xResolution) * zoom
-    # print ("xRes %.2f yRes %.2f AcrossStretch %.2f" % (xResolution, yResolution, isoStretchFactor))
+    print ("xRes %.2f yRes %.2f AcrossStretch %.2f" % (xResolution, yResolution, isoStretchFactor))
     while r.moreData():
         TypeOfDatagram, datagram = r.readDatagram()
         if (TypeOfDatagram == 0):
@@ -130,7 +127,7 @@ def createWaterfall(filename, colors, beamCount, shadeScale=1, zoom=1.0, xResolu
             maxDepth = max(maxDepth, max(datagram.Depth))
 
             # we need to stretch the data to make it isometric, so lets use numpy interp routing to do that for Us
-            xp = np.array(datagram.AcrossTrackDistance) #the x distance for the beams of a ping.  we could possibly use teh real values here instead todo
+            xp = np.array(datagram.AcrossTrackDistance) #the x distance for the beams of a ping.  we could possibly use the real values here instead todo
             fp = np.array(datagram.Depth) #the depth list as a numpy array
             x = np.linspace(leftExtent, rightExtent, outputResolution) #the required samples needs to be about the same as the original number of samples, spread across the across track range
             newDepths = np.interp(x, xp, fp, left=0.0, right=0.0)
@@ -141,23 +138,24 @@ def createWaterfall(filename, colors, beamCount, shadeScale=1, zoom=1.0, xResolu
             percentageRead = (recCount / totalrecords) 
             update_progress("Decoding .all file", percentageRead)
     update_progress("Decoding .all file", 1)
+    r.close()    
+
+    # we have all data loaded, so now lets make a waterfall image...
 
     print ("correcting for vessel speed...")
     # we now need to interpolate in the along track direction so we have apprximate isometry
     npGrid = np.array(waterfall)
-    # npGrid = np.ma.masked_values(npGrid, 0.0)
 
     stretchedGrid = np.empty((0, int(len(npGrid) * isoStretchFactor)))    
     for column in npGrid.T:
         y = np.linspace(0, len(column), len(column) * isoStretchFactor) #the required samples
         yp = np.arange(len(column)) 
         w2 = np.interp(y, yp, column, left=0.0, right=0.0)
-        # w2 = np.interp(y, yp, column, left=None, right=None)
         stretchedGrid = np.append(stretchedGrid, [w2],axis=0)
     npGrid = stretchedGrid
     npGrid = np.ma.masked_values(npGrid, 0.0)
-    # meanDepth = np.average(waterfall)
-    # print ("Mean Depth %.2f" % meanDepth)
+    meanDepth = np.average(waterfall)
+    print ("Mean Depth %.2f" % meanDepth)
     print ("color mapping...")
     
     if gray:
@@ -182,16 +180,15 @@ def createWaterfall(filename, colors, beamCount, shadeScale=1, zoom=1.0, xResolu
         # now blend the two images
         img = ImageChops.subtract(colorImage, img).convert('RGB')
 
-    #rotate the image if the user requests this.  It is a little better for viewing in a browser
-    annotateWaterfall(img, navigation, isoStretchFactor)
+    if annotate:
+        #rotate the image if the user requests this.  It is a little better for viewing in a browser
+        annotateWaterfall(img, navigation, isoStretchFactor)
+
     if rotate:
         img = img.rotate(-90, expand=True)
     img.save(os.path.splitext(filename)[0]+'.png')
     print ("Saved to: ", os.path.splitext(filename)[0]+'.png')
 
-    # r.rewind()
-    # print("Complete converting ALL file to waterfall :-)")
-    r.close()    
 
 def annotateWaterfall(img, navigation, scaleFactor):
     '''loop through the navigation and annotate'''
