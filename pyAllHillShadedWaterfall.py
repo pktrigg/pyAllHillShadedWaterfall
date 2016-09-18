1469d1fd1a5b21e8d7d2cbb2feaff1b64b421faa
PKLaptop <p.kennedy@fugro.com>
2016-09-18 23:30:31 +0800

    more testing

--- pyAllHillShadedWaterfall.py
+++ pyAllHillShadedWaterfall.py
@@ -25,7 +25,7 @@ def main():
     parser.add_argument('-i', dest='inputFile', action='store', help='-i <ALLfilename> : input ALL filename to image. It can also be a wildcard, e.g. *.all')
     parser.add_argument('-s', dest='shadeScale', default = 0, action='store', help='-s <value> : Override Automatic Shade scale factor with this value. A smaller number (0.1) provides less shade that a larger number (10) Range is anything.  [Default: 0]')
     parser.add_argument('-z', dest='zoom', default = 1.0, action='store', help='-z <value> : Zoom scale factor. A larger number makes a larger image, and a smaller number (0.5) provides a smaller image, e.g -z 2 makes an image twice the native resolution. [Default: 1.0]')
-    parser.add_argument('-a', action='store_true', default=True, dest='annotate', help='-a : Annotate the image with timestamps.  [Default: True]')
+    parser.add_argument('-a', action='store_true', default=False, dest='annotate', help='-a : Annotate the image with timestamps.  [Default: True]')
     parser.add_argument('-r', action='store_true', default=False, dest='rotate', help='-r : Rotate the resulting waterfall so the image reads from left to right instead of bottom to top.  [Default is bottom to top]')
     parser.add_argument('-gray', action='store_true', default=False, dest='gray', help='-gray : Apply a gray scale depth palette to the image instead of a color depth.  [Default is False]')
 
@@ -40,9 +40,10 @@ def main():
     print ("processing with settings: ", args)
     for filename in glob(args.inputFile):
         xResolution, yResolution, beamCount, leftExtent, rightExtent, distanceTravelled, navigation = computeXYResolution(filename)
-        print("XRes %.2f YRes %.2f beamCount %d leftExtent %.2f, rightExtent %.2f, swathWidth %.2f" % (xResolution, yResolution, beamCount, leftExtent, rightExtent, abs(leftExtent)+abs(rightExtent))) 
-        if (args.shadeScale==0): 
-            args.shadeScale = 35 * math.pow(abs(leftExtent)+abs(rightExtent), -0.783)
+        print("xRes %.2f yRes %.2f  leftExtent %.2f, rightExtent %.2f, distanceTravelled %.2f" % (xResolution, yResolution, leftExtent, rightExtent, distanceTravelled)) 
+        shadeScale = float(args.shadeScale)
+        if (shadeScale==0): 
+            shadeScale = 38 * math.pow(abs(leftExtent)+abs(rightExtent), -0.783)
             # args.shadeScale = 38 * math.pow(abs(leftExtent)+abs(rightExtent), -0.783)
         if beamCount == 0:
             print ("No data to process, skipping empty file")
@@ -52,7 +53,111 @@ def main():
         while (swathWidth < 300):
             zoom *= 2
             swathWidth *= zoom 
-        createWaterfall(filename, colors, beamCount, float(args.shadeScale), zoom, args.annotate, xResolution, yResolution, args.rotate, args.gray, leftExtent, rightExtent, distanceTravelled, navigation)
+        print("Shade %.2f Zoom %.2f beamCount %d swathWidth %.2f" % (shadeScale, zoom, beamCount, abs(leftExtent)+abs(rightExtent))) 
+        createWaterfall(filename, colors, beamCount, shadeScale, zoom, args.annotate, xResolution, yResolution, args.rotate, args.gray, leftExtent, rightExtent, distanceTravelled, navigation)
+
+def createWaterfall(filename, colors, beamCount, shadeScale=1, zoom=1.0, annotate=True, xResolution=1, yResolution=1, rotate=False, gray=False, leftExtent=-100, rightExtent=100, distanceTravelled=0, navigation=[]):
+    print ("Processing file: ", filename)
+
+    r = pyall.ALLReader(filename)
+    totalrecords = r.getRecordCount()
+    start_time = time.time() # time the process
+    recCount = 0
+    waterfall = []
+    minDepth = 9999.0
+    maxDepth = -minDepth
+    outputResolution = beamCount * zoom
+    isoStretchFactor = (yResolution/xResolution) * zoom
+    # print ("xRes %.2f yRes %.2f AcrossStretch %.2f" % (xResolution, yResolution, isoStretchFactor))
+    while r.moreData():
+        TypeOfDatagram, datagram = r.readDatagram()
+        if (TypeOfDatagram == 0):
+            continue
+        if (TypeOfDatagram == 'X') or (TypeOfDatagram == 'D'):
+            datagram.read()
+            if datagram.NBeams == 0:
+                continue
+
+            # if datagram.SerialNumber == 275:                    
+            for d in range(len(datagram.Depth)):
+                datagram.Depth[d] = datagram.Depth[d] + datagram.TransducerDepth
+
+            # we need to remember the actual data extents so we can set the color palette mappings to the same limits. 
+            minDepth = min(minDepth, min(datagram.Depth))
+            maxDepth = max(maxDepth, max(datagram.Depth))
+
+            # we need to stretch the data to make it isometric, so lets use numpy interp routing to do that for Us
+            xp = np.array(datagram.AcrossTrackDistance) #the x distance for the beams of a ping.  we could possibly use the real values here instead todo
+            fp = np.array(datagram.Depth) #the depth list as a numpy array
+            # fp = geodetic.medfilt(fp,31)
+            x = np.linspace(leftExtent, rightExtent, outputResolution) #the required samples needs to be about the same as the original number of samples, spread across the across track range
+            newDepths = np.interp(x, xp, fp, left=0.0, right=0.0)
+
+            # run a median filter to remove crazy noise
+            # newDepths = geodetic.medfilt(newDepths,7)
+            waterfall.insert(0, np.asarray(newDepths))            
+
+        recCount += 1
+        if r.currentRecordDateTime().timestamp() % 30 == 0:
+            percentageRead = (recCount / totalrecords) 
+            update_progress("Decoding .all file", percentageRead)
+    update_progress("Decoding .all file", 1)
+    r.close()    
+
+    # we have all data loaded, so now lets make a waterfall image...
+    #---------------------------------------------------------------    
+    print ("Correcting for vessel speed...")
+    # we now need to interpolate in the along track direction so we have apprximate isometry
+    npGrid = np.array(waterfall)
+
+    stretchedGrid = np.empty((0, int(len(npGrid) * isoStretchFactor)))    
+    for column in npGrid.T:
+        y = np.linspace(0, len(column), len(column) * isoStretchFactor) #the required samples
+        yp = np.arange(len(column)) 
+        w2 = np.interp(y, yp, column, left=0.0, right=0.0)
+        # w2 = geodetic.medfilt(w2,7)
+        
+        stretchedGrid = np.append(stretchedGrid, [w2],axis=0)
+    npGrid = stretchedGrid
+    npGrid = np.ma.masked_values(npGrid, 0.0)
+    
+    if gray:
+        print ("Hillshading...")
+        #Create hillshade a little brighter and invert so hills look like hills
+        colorMap = None
+        npGrid = npGrid.T * shadeScale * -1.0
+        hs = sr.calcHillshade(npGrid, 1, 45, 30)
+        img = Image.fromarray(hs).convert('RGBA')
+    else:
+        print ("Color mapping...")
+        npGrid = npGrid.T
+        # calculate color height map
+        cmrgb = cm.colors.ListedColormap(colors, name='from_list', N=None)
+        colorMap = cm.ScalarMappable(cmap=cmrgb)
+        colorMap.set_clim(vmin=minDepth, vmax=maxDepth)
+        colorArray = colorMap.to_rgba(npGrid, alpha=None, bytes=True)    
+        colorImage = Image.frombuffer('RGBA', (colorArray.shape[1], colorArray.shape[0]), colorArray, 'raw', 'RGBA', 0,1)
+        #Create hillshade a little darker as we are blending it. we do not need to invert as we are subtracting the shade from the color image
+        npGrid = npGrid * shadeScale 
+        hs = sr.calcHillshade(npGrid, 1, 45, 5)
+        img = Image.fromarray(hs).convert('RGBA')
+
+        # now blend the two images
+        img = ImageChops.subtract(colorImage, img).convert('RGB')
+
+    if annotate:
+        #rotate the image if the user requests this.  It is a little better for viewing in a browser
+        annotateWaterfall(img, navigation, isoStretchFactor)
+        meanDepth = np.average(waterfall)
+        waterfallPixelSize = (abs(rightExtent) + abs(rightExtent)) /  img.width
+        # print ("Mean Depth %.2f" % meanDepth)
+        imgLegend = createLegend(filename, img.width, (abs(leftExtent)+abs(rightExtent)), distanceTravelled, waterfallPixelSize, minDepth, maxDepth, meanDepth, colorMap)
+        img = spliceImages(img, imgLegend)
+
+    if rotate:
+        img = img.rotate(-90, expand=True)
+    img.save(os.path.splitext(filename)[0]+'.png')
+    print ("Saved to: ", os.path.splitext(filename)[0]+'.png')
 
 def computeXYResolution(fileName):    
     '''compute the approximate across and alongtrack resolution so we can make a nearly isometric Image'''
@@ -99,103 +204,6 @@ def computeXYResolution(fileName):
     xResolution = np.average(acrossMeans)
     yResolution = distanceTravelled / recCount
     return xResolution, yResolution, beamCount, np.min(leftExtents), np.max(rightExtents), distanceTravelled, navigation
-
-def createWaterfall(filename, colors, beamCount, shadeScale=1, zoom=1.0, annotate=True, xResolution=1, yResolution=1, rotate=False, gray=False, leftExtent=-100, rightExtent=100, distanceTravelled=0, navigation=[]):
-    print ("Processing file: ", filename)
-
-    r = pyall.ALLReader(filename)
-    totalrecords = r.getRecordCount()
-    start_time = time.time() # time the process
-    recCount = 0
-    waterfall = []
-    minDepth = 9999.0
-    maxDepth = -minDepth
-    outputResolution = beamCount * zoom
-    isoStretchFactor = (yResolution/xResolution) * zoom
-    # print ("xRes %.2f yRes %.2f AcrossStretch %.2f" % (xResolution, yResolution, isoStretchFactor))
-    while r.moreData():
-        TypeOfDatagram, datagram = r.readDatagram()
-        if (TypeOfDatagram == 0):
-            continue
-        if (TypeOfDatagram == 'X') or (TypeOfDatagram == 'D'):
-            datagram.read()
-            if datagram.NBeams == 0:
-                continue
-
-            # if datagram.SerialNumber == 275:                    
-            for d in range(len(datagram.Depth)):
-                datagram.Depth[d] = datagram.Depth[d] + datagram.TransducerDepth
-
-            # we need to remember the actual data extents so we can set the color palette mappings to the same limits. 
-            minDepth = min(minDepth, min(datagram.Depth))
-            maxDepth = max(maxDepth, max(datagram.Depth))
-
-            # we need to stretch the data to make it isometric, so lets use numpy interp routing to do that for Us
-            xp = np.array(datagram.AcrossTrackDistance) #the x distance for the beams of a ping.  we could possibly use the real values here instead todo
-            fp = np.array(datagram.Depth) #the depth list as a numpy array
-            x = np.linspace(leftExtent, rightExtent, outputResolution) #the required samples needs to be about the same as the original number of samples, spread across the across track range
-            newDepths = np.interp(x, xp, fp, left=0.0, right=0.0)
-            waterfall.insert(0, np.asarray(newDepths))            
-
-        recCount += 1
-        if r.currentRecordDateTime().timestamp() % 30 == 0:
-            percentageRead = (recCount / totalrecords) 
-            update_progress("Decoding .all file", percentageRead)
-    update_progress("Decoding .all file", 1)
-    r.close()    
-
-    # we have all data loaded, so now lets make a waterfall image...
-    #---------------------------------------------------------------    
-    print ("Correcting for vessel speed...")
-    # we now need to interpolate in the along track direction so we have apprximate isometry
-    npGrid = np.array(waterfall)
-
-    stretchedGrid = np.empty((0, int(len(npGrid) * isoStretchFactor)))    
-    for column in npGrid.T:
-        y = np.linspace(0, len(column), len(column) * isoStretchFactor) #the required samples
-        yp = np.arange(len(column)) 
-        w2 = np.interp(y, yp, column, left=0.0, right=0.0)
-        stretchedGrid = np.append(stretchedGrid, [w2],axis=0)
-    npGrid = stretchedGrid
-    npGrid = np.ma.masked_values(npGrid, 0.0)
-    
-    if gray:
-        print ("Hillshading...")
-        #Create hillshade a little brighter and invert so hills look like hills
-        colorMap = None
-        npGrid = npGrid.T * shadeScale * -1.0
-        hs = sr.calcHillshade(npGrid, 1, 45, 30)
-        img = Image.fromarray(hs).convert('RGBA')
-    else:
-        print ("Color mapping...")
-        npGrid = npGrid.T
-        # calculate color height map
-        cmrgb = cm.colors.ListedColormap(colors, name='from_list', N=None)
-        colorMap = cm.ScalarMappable(cmap=cmrgb)
-        colorMap.set_clim(vmin=minDepth, vmax=maxDepth)
-        colorArray = colorMap.to_rgba(npGrid, alpha=None, bytes=True)    
-        colorImage = Image.frombuffer('RGBA', (colorArray.shape[1], colorArray.shape[0]), colorArray, 'raw', 'RGBA', 0,1)
-        #Create hillshade a little darker as we are blending it. we do not need to invert as we are subtracting the shade from the color image
-        npGrid = npGrid * shadeScale 
-        hs = sr.calcHillshade(npGrid, 1, 45, 5)
-        img = Image.fromarray(hs).convert('RGBA')
-
-        # now blend the two images
-        img = ImageChops.subtract(colorImage, img).convert('RGB')
-
-    if annotate:
-        #rotate the image if the user requests this.  It is a little better for viewing in a browser
-        annotateWaterfall(img, navigation, isoStretchFactor)
-        meanDepth = np.average(waterfall)
-        waterfallPixelSize = (abs(rightExtent) + abs(rightExtent)) /  img.width
-        # print ("Mean Depth %.2f" % meanDepth)
-        imgLegend = createLegend(filename, img.width, (abs(leftExtent)+abs(rightExtent)), distanceTravelled, waterfallPixelSize, minDepth, maxDepth, meanDepth, colorMap)
-        img = spliceImages(img, imgLegend)
-
-    if rotate:
-        img = img.rotate(-90, expand=True)
-    img.save(os.path.splitext(filename)[0]+'.png')
-    print ("Saved to: ", os.path.splitext(filename)[0]+'.png')
 
 def annotateWaterfall(img, navigation, scaleFactor):
     '''loop through the navigation and annotate'''
